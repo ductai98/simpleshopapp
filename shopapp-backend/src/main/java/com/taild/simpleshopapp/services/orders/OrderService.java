787177556afd0e024dfcf82441be77d22009ab07
taild5 <taild5@fpt.com>
2025-05-23 01:50:14 +0700

    create order

--- shopapp-backend/src/main/java/com/taild/simpleshopapp/services/orders/OrderService.java
+++ shopapp-backend/src/main/java/com/taild/simpleshopapp/services/orders/OrderService.java
@@ -1,20 +1,28 @@
 package com.taild.simpleshopapp.services.orders;
 
+import com.taild.simpleshopapp.dtos.orders.CartItemDTO;
 import com.taild.simpleshopapp.dtos.orders.OrderDTO;
 import com.taild.simpleshopapp.dtos.orders.OrderResponse;
+import com.taild.simpleshopapp.enums.OrderStatus;
 import com.taild.simpleshopapp.exceptions.DataNotFoundException;
 import com.taild.simpleshopapp.models.Order;
+import com.taild.simpleshopapp.models.OrderDetail;
+import com.taild.simpleshopapp.models.Product;
 import com.taild.simpleshopapp.models.User;
 import com.taild.simpleshopapp.repositories.OrderDetailRepository;
 import com.taild.simpleshopapp.repositories.OrderRepository;
 import com.taild.simpleshopapp.repositories.ProductRepository;
 import com.taild.simpleshopapp.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,12 +32,70 @@ public class OrderService implements IOrderService{
     private final OrderRepository orderRepository;
     private final ProductRepository productRepository;
     private final OrderDetailRepository orderDetailRepository;
+    private final ModelMapper modelMapper;
 
     @Override
     @Transactional
     public Order createOrder(OrderDTO orderDTO) throws Exception {
 
-        return null;
+        User user = userRepository
+                .findById(orderDTO.getUserId())
+                .orElseThrow(() -> new DataNotFoundException("Cannot find user with id: "+orderDTO.getUserId()));
+        //convert orderDTO => Order
+        //dùng thư viện Model Mapper
+        // Tạo một luồng bảng ánh xạ riêng để kiểm soát việc ánh xạ
+        modelMapper.typeMap(OrderDTO.class, Order.class)
+                .addMappings(mapper -> mapper.skip(Order::setId));
+        // Cập nhật các trường của đơn hàng từ orderDTO
+        Order order = new Order();
+        modelMapper.map(orderDTO, order);
+        order.setUser(user);
+        order.setOrderDate(LocalDateTime.now());//lấy thời điểm hiện tại
+        order.setStatus(OrderStatus.PENDING);
+        //Kiểm tra shipping date phải >= ngày hôm nay
+        LocalDate shippingDate = orderDTO.getShippingDate() == null
+                ? LocalDate.now() : orderDTO.getShippingDate();
+        if (shippingDate.isBefore(LocalDate.now())) {
+            throw new DataNotFoundException("Date must be at least today !");
+        }
+
+        order.setShippingDate(shippingDate);
+        order.setActive(true);//đoạn này nên set sẵn trong sql
+        //EAV-Entity-Attribute-Value model
+        order.setTotalMoney(orderDTO.getTotalMoney());
+
+        if(orderDTO.getShippingAddress() == null) {
+            order.setShippingAddress(orderDTO.getAddress());
+        }
+        // Tạo danh sách các đối tượng OrderDetail từ cartItems
+        List<OrderDetail> orderDetails = new ArrayList<>();
+        for (CartItemDTO cartItemDTO : orderDTO.getCartItems()) {
+            // Tạo một đối tượng OrderDetail từ CartItemDTO
+            OrderDetail orderDetail = new OrderDetail();
+            orderDetail.setOrder(order);
+
+            // Lấy thông tin sản phẩm từ cartItemDTO
+            Long productId = cartItemDTO.getProductId();
+            int quantity = cartItemDTO.getQuantity();
+
+            // Tìm thông tin sản phẩm từ cơ sở dữ liệu (hoặc sử dụng cache nếu cần)
+            Product product = productRepository.findById(productId)
+                    .orElseThrow(() -> new DataNotFoundException("Product not found with id: " + productId));
+
+            // Đặt thông tin cho OrderDetail
+            orderDetail.setProduct(product);
+            orderDetail.setNumberOfProducts(quantity);
+            // Các trường khác của OrderDetail nếu cần
+            orderDetail.setPrice(product.getPrice());
+
+            // Thêm OrderDetail vào danh sách
+            orderDetails.add(orderDetail);
+        }
+
+        // Lưu danh sách OrderDetail vào cơ sở dữ liệu
+        orderDetailRepository.saveAll(orderDetails);
+        orderRepository.save(order);
+        return order;
     }
 
     @Override
@@ -113,10 +179,11 @@ public class OrderService implements IOrderService{
             orderRepository.save(order);
         }
     }
+
     @Override
     public List<OrderResponse> findByUserId(Long userId) {
         List<Order> orders = orderRepository.findByUserId(userId);
-        return orders.stream().map(order -> OrderResponse.fromOrder(order)).toList();
+        return orders.stream().map(OrderResponse::fromOrder).toList();
     }
 
     @Override
