39454637803f08d416839ba4519219a5d0cb6bc2
taild5 <taild5@fpt.com>
2025-07-01 02:15:36 +0700

    front-end: add home component

---
+++ shopapp-frontend/src/app/app-module.ts
@@ -0,0 +1,22 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { App } from './app';
+import { Home } from './home/home';
+
+@NgModule({
+  declarations: [
+    App,
+    Home,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
